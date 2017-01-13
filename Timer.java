6170b2655aa90bdce7f1cb3510a5e50263011ca4
Rammurthy Mudimadugula <rammurthy.mudimadugula@gmail.com>
2017-01-13 00:35:07 -0600

    Program to start and stop Java Timer

---
+++ Timer.java
@@ -0,0 +1,33 @@
+
+/**
+ * Timer class for roughly calculating running time of programs
+ *
+ * @author Rammurthy Mudimadugula
+ * @version 1.0
+ * 
+ */
+
+public class Timer {
+    long startTime, endTime, elapsedTime, memAvailable, memUsed;
+
+    Timer() {
+	startTime = System.currentTimeMillis();
+    }
+
+    public void start() {
+	startTime = System.currentTimeMillis();
+    }
+
+    public Timer end() {
+	endTime = System.currentTimeMillis();
+	elapsedTime = endTime-startTime;
+	memAvailable = Runtime.getRuntime().totalMemory();
+	memUsed = memAvailable - Runtime.getRuntime().freeMemory();
+	return this;
+    }
+
+    public String toString() {
+        return "Time: " + elapsedTime + " msec.\n" + "Memory: " + (memUsed/1048576) + " MB / " + (memAvailable/1048576) + " MB.";
+    }
+
+}
